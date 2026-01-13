51b63abe0724663fabeb9f53757195cfe715b767
albavera <albvercab@alu.edu.gva.es>
2026-01-13 08:51:01 +0100

    Conductor

--- src/Libro.java
+++ src/Libro.java
@@ -4,39 +4,15 @@ public class Libro {
     private int numPaginas;
     private int valoracion;
 
+    public Libro(String titulo, String autor, int numPaginas, int valoracion) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numPaginas = numPaginas;
+        this.valoracion = valoracion;
+
+    }
+
     void mostrarInfo() {
         System.out.println("El libro '" + titulo + "' de " + autor + " tiene " + numPaginas + " páginas, con una valoración de " + valoracion + " sobre 10.");
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
-
-    public void setNumPaginas(int numPaginas) {
-        this.numPaginas = numPaginas;
-    }
-
-    public void setValoracion(int valoracion) {
-        this.valoracion = valoracion;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public String getAutor() {
-        return autor;
-    }
-
-    public int getNumPaginas() {
-        return numPaginas;
-    }
-
-    public int getValoracion() {
-        return valoracion;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Libro libro1 = new Libro();
-        libro1.setTitulo("Quijote");
-        libro1.setAutor("Ceravntes");
-        libro1.setNumPaginas(500);
-        libro1.setValoracion(9);
+        Libro libro1 = new Libro("Quijote", "Cervantes", 500, 9);
 
         libro1.mostrarInfo();
     }
