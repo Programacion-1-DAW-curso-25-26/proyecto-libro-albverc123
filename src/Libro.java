7dfdc90db7ac2d0c6674bb9988fca95feb06446d
albavera <albvercab@alu.edu.gva.es>
2026-01-20 09:10:31 +0100

    Adiciones hechas

--- src/Libro.java
+++ src/Libro.java
@@ -1,16 +1,87 @@
+import java.util.Objects;
+
 public class Libro {
+
+    // --- ATRIBUTOS PÚBLICOS ---
+
+   /*
+   String titulo;
+   String autor;
+   int numPaginas;
+   int valoracion;
+   */
+
+    // ------------------------------
+
+
+    // --- ATRIBUTOS PRIVADOS ---
+
     private String titulo;
     private String autor;
     private int numPaginas;
     private int valoracion;
+    private int isbn;
 
-    public Libro(String titulo, String autor, int numPaginas, int valoracion) {
+   /*
+   public void setTitulo(String titulo) {
+       this.titulo = titulo;
+   }
+
+   public void setAutor(String autor) {
+       this.autor = autor;
+   }
+
+   public void setNumPaginas(int numPaginas) {
+       this.numPaginas = numPaginas;
+   }
+
+   public void setValoracion(int valoracion) {
+       this.valoracion = valoracion;
+   }
+    */
+
+    // ------------------------------
+
+    // --- CONDUCTORES ---
+
+    public Libro(String titulo, String autor, int numPaginas, int valoracion, int isbn) {
         this.titulo = titulo;
         this.autor = autor;
         this.numPaginas = numPaginas;
         this.valoracion = valoracion;
-
+        this.isbn = isbn;
     }
+    // ------------------------------
+
+    // --- MÁS MÉTODOS ---
+
+    // --- equals & haschCode ---
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Libro libro = (Libro) o;
+        return isbn == libro.isbn;
+    }
+    // -- el hashCode no se va a ver
+    /* @Override
+    public int hashCode() {
+        return Objects.hashCode(isbn);
+    }
+    */
+
+    // --- toString ---
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", numPaginas=" + numPaginas +
+                ", valoracion=" + valoracion +
+                ", isbn=" + isbn +
+                '}';
+    }
+
+    // ------------------------------
 
     void mostrarInfo() {
         System.out.println("El libro '" + titulo + "' de " + autor + " tiene " + numPaginas + " páginas, con una valoración de " + valoracion + " sobre 10.");
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,42 @@
 public class Main {
     public static void main(String[] args) {
-        Libro libro1 = new Libro("Quijote", "Cervantes", 500, 9);
+
+
+        // --- ATRIBUTOS PÚBLICOS ---
+
+       /*
+       Libro libro0 = new Libro();
+       libro0.titulo("Quijote");
+       libro0.autor("Cervantes");
+       libro0.numPaginas(500);
+       libro0.valoracion(9);
+       */
+
+
+        // --- ATRIBUTOS PRIVADOS ---
+
+       /*
+       Libro libro0 = new Libro();
+       libro0.setTitulo("Quijote");
+       libro0.setAutor("Cervantes");
+       libro0.setNumPaginas(900);
+       libro0.setValoracion(9);
+       */
+
+
+        // --- CONDUCTORES ---
+
+        Libro libro1 = new Libro("Quijote", "Cervantes", 500, 9, 11);
+        Libro libro2 = new Libro("Quijote", "Cervantes", 300, 7, 11);
 
         libro1.mostrarInfo();
+        if (libro1.equals(libro2)) {
+            System.out.println("Son iguales");
+        }else {
+            System.out.println("No son iguales");
+        }
+        System.out.println(libro1);
+        System.out.println(libro2);
+
     }
 }
